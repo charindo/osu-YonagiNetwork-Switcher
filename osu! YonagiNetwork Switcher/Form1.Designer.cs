28defa86e7466bf5a0034c47aa9db0f9d480d87d
charindo <58286149+charindo@users.noreply.github.com>
2021-01-21 01:23:44 +0900

    Re-upload

---
+++ osu! YonagiNetwork Switcher/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace osu__YonagiNetwork_Switcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(494, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Switch server: YonagiNetwork";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(494, 83);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Switch server: osu!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 199);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "osu! YonagiNetwok Switcher v1.0.1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ osu! YonagiNetwork Switcher/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace osu__YonagiNetwork_Switcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            remove_hosts_data();
+            add_hosts_data();
+            MessageBox.Show("Changed the server to YonagiNetwork!\r\nPlease restart osu!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            remove_hosts_data();
+            MessageBox.Show("Changed the server to osu!\r\nPlease restart osu!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void add_hosts_data()
+        {
+            string path = @"c:/Windows/System32/drivers/etc/hosts";
+
+            //ファイルの内容を読み込む
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+
+            //内容をすべて読み込む
+            string s = sr.ReadToEnd();
+
+            //閉じる
+            sr.Close();
+
+            // 文字列置換
+            string host = "121.107.10.216";
+            s += "\r\n" + host + " osu.ppy.sh";
+            s += "\r\n" + host + " c.ppy.sh";
+            s += "\r\n" + host + " c1.ppy.sh";
+            s += "\r\n" + host + " c2.ppy.sh";
+            s += "\r\n" + host + " c3.ppy.sh";
+            s += "\r\n" + host + " c4.ppy.sh";
+            s += "\r\n" + host + " c5.ppy.sh";
+            s += "\r\n" + host + " c6.ppy.sh";
+            s += "\r\n" + host + " ce.ppy.sh";
+            s += "\r\n" + host + " a.ppy.sh";
+            s += "\r\n" + host + " i.ppy.sh";
+            s += "\r\n" + host + " i-need-to.click";
+            s += "\r\n" + host + " c.i-need-to.click";
+            s += "\r\n" + host + " i.i-need-to.click";
+            s += "\r\n" + host + " a.i-need-to.click";
+            s += "\r\n" + host + " old.i-need-to.click";
+
+            //Shift JISで書き込む
+            //書き込むファイルが既に存在している場合は、上書きする
+            StreamWriter sw = new StreamWriter(
+                path,
+                false,
+                Encoding.GetEncoding("Shift_JIS"));
+            //TextBox1.Textの内容を書き込む
+            sw.Write(s);
+            //閉じる
+            sw.Close();
+        }
+
+        private void remove_hosts_data()
+        {
+            string path = @"c:/Windows/System32/drivers/etc/hosts";
+
+            //ファイルの内容を読み込む
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+
+            //内容をすべて読み込む
+            string s = sr.ReadToEnd();
+
+            //閉じる
+            sr.Close();
+
+            // 文字列置換
+            string host = "121.107.10.216";
+            s = s.Replace("\r\n"+host+" osu.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c1.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c2.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c3.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c4.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c5.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c6.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" ce.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" a.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" i.ppy.sh", "");
+            s = s.Replace("\r\n" + host + " i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " c.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " i.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " a.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " old.i-need-to.click", "");
+
+            //Shift JISで書き込む
+            //書き込むファイルが既に存在している場合は、上書きする
+            StreamWriter sw = new StreamWriter(
+                path,
+                false,
+                Encoding.GetEncoding("Shift_JIS"));
+            //TextBox1.Textの内容を書き込む
+            sw.Write(s);
+            //閉じる
+            sw.Close();
+        }
+    }
+}
